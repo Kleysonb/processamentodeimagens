05bed0d5f23517f09919a472f62e95c28159cd1f
Kleyson Morais <kleysonb@hotmail.com>
2017-09-03 11:31:19 -0300

    Base do Projeto Algoritmo de Rotulação

---
+++ rotulacao/src/sample/Controller.java
@@ -0,0 +1,49 @@
+package sample;
+
+import javafx.embed.swing.SwingFXUtils;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+    @FXML
+    private Button abrirArquivo;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+    }
+
+    @FXML
+    private void seletorArquivo() {
+        //System.out.println("oii");
+        Stage mainStage = new Stage();
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Abrir Arquivo");
+        try{
+            boolean b = fileChooser.getExtensionFilters().addAll(
+                    new FileChooser.ExtensionFilter("Imagem", "*.png", "*.jpg", "*.jpeg"));
+            File selectedFile = fileChooser.showOpenDialog(mainStage);
+            if (selectedFile != null) {
+                System.out.println("Arquivo Enviado para Interpolação.");
+
+            }
+            BufferedImage bufferedImage = ImageIO.read(selectedFile);
+            Image image = SwingFXUtils.toFXImage(bufferedImage, null);
+        } catch (IOException ex) {
+            System.out.println("Error");
+        }
+
+    }
+
+}
